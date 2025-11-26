67b25ff8f2fec862096f0242f587e2ab185f8247
Francisco Daniel <francisco.daniel.108@ufrn.edu.br>
2025-11-26 08:58:41 -0300

    fix: corrigi rotas de product no fetch

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,5 +1,5 @@
 async function getProdutos() {
-  const res = await fetch("http://localhost:3000/api/products", {
+  const res = await fetch("/api/products", {
     cache: "no-store",
   });
   return res.json();
